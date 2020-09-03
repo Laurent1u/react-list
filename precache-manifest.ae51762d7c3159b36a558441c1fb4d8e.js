bb2c4b43991bb3ef7d72e60891e7319df83b55c8
larry <pascu.laurentiu17@gmail.com>
2020-09-03 07:15:19 +0300

    Updates

---
+++ precache-manifest.ae51762d7c3159b36a558441c1fb4d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c1c8376b868f80069ef5d932ac3fb2",
+    "url": "/react-list/index.html"
+  },
+  {
+    "revision": "f725f2329383e87cf8ea",
+    "url": "/react-list/static/css/main.a0a0557a.chunk.css"
+  },
+  {
+    "revision": "262c788b11e23e4bffdf",
+    "url": "/react-list/static/js/2.84396668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-list/static/js/2.84396668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f725f2329383e87cf8ea",
+    "url": "/react-list/static/js/main.7e02e29f.chunk.js"
+  },
+  {
+    "revision": "86cb9d92f84477a26d68",
+    "url": "/react-list/static/js/runtime-main.7f8239f2.js"
+  }
+]);
