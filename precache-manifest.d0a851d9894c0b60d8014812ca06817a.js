c2f74fd08deed4343901425df5f57a1d1a25f57a
larry <pascu.laurentiu17@gmail.com>
2020-09-03 07:37:10 +0300

    restore files

---
+++ precache-manifest.d0a851d9894c0b60d8014812ca06817a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ad9d0c0f5ea04fa9a99645c27679f2",
+    "url": "/react-list/index.html"
+  },
+  {
+    "revision": "c4a77549b6cd531794b7",
+    "url": "/react-list/static/css/main.a0a0557a.chunk.css"
+  },
+  {
+    "revision": "262c788b11e23e4bffdf",
+    "url": "/react-list/static/js/2.84396668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-list/static/js/2.84396668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a77549b6cd531794b7",
+    "url": "/react-list/static/js/main.d3565b5e.chunk.js"
+  },
+  {
+    "revision": "86cb9d92f84477a26d68",
+    "url": "/react-list/static/js/runtime-main.7f8239f2.js"
+  }
+]);
