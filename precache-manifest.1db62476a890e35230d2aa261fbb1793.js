f9d79dcde042c4ca17447844bc0b66ac6740161e
larry <pascu.laurentiu17@gmail.com>
2020-09-03 07:58:34 +0300

    Updates

--- precache-manifest.1db62476a890e35230d2aa261fbb1793.js
+++ precache-manifest.1db62476a890e35230d2aa261fbb1793.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17bfdebf4a4ce559ed1ab285eb3d20cd",
+    "revision": "64074fd3514404032e7d793cc1b33073",
     "url": "/react-list/index.html"
   },
   {
-    "revision": "d53d5411dffc9662da68",
+    "revision": "03275e3d5a9762550498",
     "url": "/react-list/static/css/main.b55d508f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-list/static/js/2.84396668.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d53d5411dffc9662da68",
-    "url": "/react-list/static/js/main.d3565b5e.chunk.js"
+    "revision": "03275e3d5a9762550498",
+    "url": "/react-list/static/js/main.f1a7436a.chunk.js"
   },
   {
     "revision": "86cb9d92f84477a26d68",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-list/precache-manifest.92629abfad9b3105d0e0bf88b3fb6096.js"
+  "/react-list/precache-manifest.1db62476a890e35230d2aa261fbb1793.js"
 );
 
 self.addEventListener('message', (event) => {
